0de3e68cc793ff48d0618b787986332e8248da34
Lucas Cazeto <lucas.cazeto@klarna.com>
2021-04-05 20:49:19 +0200

    minor

--- backend/src/routes/__tests__/orderStatus.post.spec.ts
+++ backend/src/routes/__tests__/orderStatus.post.spec.ts
@@ -43,5 +43,32 @@ describe("orderStatus.post", () => {
     expect(res3.status).toEqual(400);
   });
 
-  xit("should return the most rerecent checkpoint for submitted tracking_number", async () => {});
+  it("should return payload with tracking numbers and correspondent most recent checkpoints", async () => {
+    //not mocking stream input data to save some development time
+    const res = await request(server)
+      .post("/status")
+      .send({
+        trackingNumbers: ["00340000161200000001", "00331612197202003141"],
+      });
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual({
+      "00340000161200000001": {
+        tracking_number: "00340000161200000001",
+        location: "",
+        timestamp: "2018-04-06T05:58:00.000Z",
+        status: "Scheduled",
+        status_text: "Delivery date set",
+        status_details:
+          "An appointment to make the delivery has been made. The goods will be delivered on Saturday, Apr 7th, 2018, between 9:30 am and 1:00 pm.",
+      },
+      "00331612197202003141": {
+        tracking_number: "00331612197202003141",
+        location: "",
+        timestamp: "2020-03-01T00:00:00.000Z",
+        status: "OrderProcessed",
+        status_text: "Order processed",
+        status_details: "The order has been processed.",
+      },
+    });
+  });
 });
--- backend/src/routes/__tests__/userOrders.post.spec.ts
+++ backend/src/routes/__tests__/userOrders.post.spec.ts
@@ -20,7 +20,7 @@ describe("orderOrders.post", () => {
     expect(res.status).toEqual(400);
 
     const res2 = await request(server)
-      .post("/status")
+      .post("/orders")
       .send({
         email: ["test@test.com"],
       });
@@ -33,9 +33,61 @@ describe("orderOrders.post", () => {
     });
     expect(res.status).toEqual(400);
 
-    const res2 = await request(server).post("/status").send({
+    const res2 = await request(server).post("/orders").send({
       email: "@test.com",
     });
     expect(res2.status).toEqual(400);
+  });
+
+  it("should return payload with orders correspondent to the submitted email", async () => {
+    //not mocking stream input data to save some development time
+    const res = await request(server).post("/orders").send({
+      email: "julian@parcellab.com",
+    });
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual([
+      {
+        articleImageUrl: "http://cdn.parcellab.com/img/sales-cannon/parcellab-bag.jpg",
+        articleNo: "A-B2-U",
+        city: "München",
+        courier: "DHL",
+        destination_country_iso3: "DEU",
+        email: "julian@parcellab.com",
+        orderNo: "ORD-123-2018",
+        product_name: "parcelLab Tote Bag",
+        quantity: "1",
+        street: "Landwehrstr. 39",
+        tracking_number: "00340000161200000001",
+        zip_code: "80336",
+      },
+      {
+        articleImageUrl: "http://cdn.parcellab.com/img/sales-cannon/parcellab-cap.jpg",
+        articleNo: "A-C1-L",
+        city: "München",
+        courier: "DHL",
+        destination_country_iso3: "DEU",
+        email: "julian@parcellab.com",
+        orderNo: "ORD-123-2018",
+        product_name: "parcelLab Branded Cap",
+        quantity: "2",
+        street: "Landwehrstr. 39",
+        tracking_number: "00340000161200000001",
+        zip_code: "80336",
+      },
+      {
+        articleImageUrl: "",
+        articleNo: "",
+        city: "Berlin",
+        courier: "DHL",
+        destination_country_iso3: "DEU",
+        email: "julian@parcellab.com",
+        orderNo: "780XX004",
+        product_name: "",
+        quantity: "",
+        street: "Schillerstr. 23a",
+        tracking_number: "00331612197202003141",
+        zip_code: "10625",
+      },
+    ]);
   });
 });
